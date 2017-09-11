110fe851611f3cba5ea9af7f6d3040647bd7befd
charmingelle <revenkoaa89@gmail.com>
2017-09-12 00:04:27 +0300

    Added new utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,26 @@
+import {
+  REPO_NAME_SORT,
+  STARS_COUNT_SORT,
+  OPEN_ISSUES_SORT,
+  UPDATE_DATE_SORT
+} from './components/SortingPanel';
+
+function createSorter(key) {
+  return function(arr, order) {
+    arr.sort((a, b) => {
+      if (a[key] > b[key]) return order;
+      if (a[key] < b[key]) return -order;
+    });
+  };
+}
+
+export const cardSorters = {
+  [REPO_NAME_SORT]: createSorter('name'),
+  [STARS_COUNT_SORT]: createSorter('stargazers_count'),
+  [OPEN_ISSUES_SORT]: createSorter('open_issues_count'),
+  [UPDATE_DATE_SORT]: createSorter('updated_at')
+};
+
 export function getPageNumber(reposAmount) {
   const MAX_REPOS_ON_PAGE = 30;
 
@@ -11,72 +34,42 @@ export function isScrolledToBottom() {
   );
 }
 
-export function sortByRepoName(arr, order) {
-  arr.sort(function(a, b) {
-    if (a.name.toLowerCase() > b.name.toLowerCase()) return order;
-    if (a.name.toLowerCase() < b.name.toLowerCase()) return -order;
-  });
-}
-
-export function sortByStarsCount(arr, order) {
-  arr.sort(function(a, b) {
-    if (a.stargazers_count > b.stargazers_count) return order;
-    if (a.stargazers_count < b.stargazers_count) return -order;
-  });
-}
-
-export function sortByOpenIssuesCount(arr, order) {
-  arr.sort(function(a, b) {
-    if (a.open_issues_count > b.open_issues_count) return order;
-    if (a.open_issues_count < b.open_issues_count) return -order;
-  });
-}
-
-export function sortByUpdatedDate(arr, order) {
-  arr.sort(function(a, b) {
-    if (a.updated_at > b.updated_at) return order;
-    if (a.updated_at < b.updated_at) return -order;
-  });
-}
-
 export function filterByPresentOpenIssues(arr) {
-  return arr.filter(function(element) {
-    return element.open_issues_count > 0;
-  });
+  return arr.filter(element => element.open_issues_count > 0);
 }
 
 export function filterByPresentTopics(arr) {
-  return arr.filter(function(element) {
-    return Array.isArray(element.topics) && element.topics.length;
-  });
+  return arr.filter(
+    element => Array.isArray(element.topics) && element.topics.length
+  );
 }
 
 export function filterByStarred(arr, times) {
-  return arr.filter(function(element) {
-    return element.stargazers_count >= times;
-  });
+  return arr.filter(element => element.stargazers_count >= times);
 }
 
 export function filterByUpdatedAfter(arr, date) {
-  return arr.filter(function(element) {
-    return element.updated_at - date > 0;
-  });
+  return arr.filter(element => element.updated_at - date > 0);
 }
 
 export function filterForks(arr) {
-  return arr.filter(function(element) {
-    return element.fork === true;
-  });
+  return arr.filter(element => element.fork === true);
 }
 
 export function filterSources(arr) {
-  return arr.filter(function(element) {
-    return element.fork === false;
-  });
+  return arr.filter(element => element.fork === false);
 }
 
 export function filterByLanguage(arr, language) {
-  return arr.filter(function(element) {
-    return element.language == language;
-  });
+  return arr.filter(element => element.language === language);
+}
+
+export function clean(domElement) {
+  while (domElement.firstChild) {
+    domElement.removeChild(domElement.firstChild);
+  }
+}
+
+export function appendChildren(target, children) {
+  children.forEach(child => target.appendChild(child));
 }
